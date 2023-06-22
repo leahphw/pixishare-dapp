8781ea989714534abbeb171e8158631782b18dae
Leah Le <phnganh.le@gmail.com>
2023-06-22 17:24:16 +0700

    test create posts

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -2,4 +2,22 @@ pragma solidity ^0.5.0;
 
 contract Decentragram {
   string public name = "Decentragram";
+
+  // Store posts
+  struct Post {
+    uint id;
+    string hash;
+    string description;
+    uint tipAmount;
+    address payable author;
+  }
+
+  mapping(uint => Post) public posts;
+
+  // Create posts
+  function upload() public {
+    posts[1] = Post(1, "abc", "hello", 0, address(0x0));
+  }
+
+  // Tip posts
 }
