11caef3000338917665c5bfa5a615f8dbb317650
Leah Le <phnganh.le@gmail.com>
2023-06-25 19:08:12 +0700

    add contract functionality

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -12,12 +12,59 @@ contract Decentragram {
     address payable author;
   }
 
+  event PostCreated(
+    uint id,
+    string hash,
+    string description,
+    uint tipAmount,
+    address payable author
+  );
+
+  event PostTipped(
+    uint id,
+    string hash,
+    string description,
+    uint tipAmount,
+    address payable author
+  );
+
   mapping(uint => Post) public posts;
+  uint public postCount = 0;
 
   // Create posts
-  function upload() public {
-    posts[1] = Post(1, "abc", "hello", 0, address(0x0));
+  function upload(string memory _postHash, string memory _description) public {
+    // Set requirements
+    require(bytes(_description).length > 0);
+    require(bytes(_postHash).length > 0);
+    require(msg.sender != address(0x0));
+
+    // Increment count
+    postCount++;
+    posts[postCount] = Post(postCount, _postHash, _description, 0, msg.sender);
+
+    // Trigger an event
+    emit PostCreated(postCount, _postHash, _description, 0, msg.sender);
   }
 
   // Tip posts
+  function tipUser(uint _id) public payable {
+    // Ser requirements
+    require(_id > 0 && _id <= postCount);
+
+    // Fetch post and author
+    Post memory _post = posts[_id];
+    address payable _author = _post.author;
+
+    // Send ETH to author
+    address(_author).transfer(msg.value);
+
+    // Update tip amount
+    _post.tipAmount = msg.value + _post.tipAmount;
+
+    // Update post
+    posts[_id] = _post;
+
+    // Trigger an event
+    emit PostCreated(_id, _post.hash, _post.description, _post.tipAmount, _author);
+  }
 }
--- test/test.js
+++ test/test.js
@@ -1,3 +1,5 @@
+const { assert } = require('chai')
+
 const Decentragram = artifacts.require('./Decentragram.sol')
 
 require('chai')
@@ -28,12 +30,71 @@ contract('Decentragram', ([deployer, author, tipper]) => {
 
   describe('posts', async () => {
     let result;
+    const hash = "abcd1234"
 
-    it('create images', async () => {
-      result = await decentragram.upload()
-      let post = await decentragram.posts(1)
-      console.log(post)
+    before(async () => {
+      result = await decentragram.upload(hash, "post description", { from: author })
+      postCount = await decentragram.postCount()
     })
+
+    it('create posts', async () => {
+
+      // Success
+      assert.equal(postCount, 1)
+      const event = result.logs[0].args
+      assert.equal(event.id.toNumber(), postCount.toNumber(), "id is correct")
+      assert.equal(event.hash, hash, "hash is correct")
+      assert.equal(event.description, "post description", "description is correct")
+      assert.equal(event.tipAmount, 0, "tip amount is correct")
+      assert.equal(event.author, author, "author is correct")
+
+      // Fail
+      await decentragram.upload('', 'post description', { from: author }).should.be.rejected;
+      await decentragram.upload(hash, '', { from: author }).should.be.rejected;
+      await decentragram.upload(hash, 'post description', { from: 0x0 }).should.be.rejected;
+    })
+
+    it('list posts', async () => {
+      const post = await decentragram.posts(postCount);
+      assert.equal(post.id.toNumber(), postCount.toNumber(), "id is correct")
+      assert.equal(post.hash, hash, "hash is correct")
+      assert.equal(post.description, "post description", "description is correct")
+      assert.equal(post.tipAmount, 0, "tip amount is correct")
+      assert.equal(post.author, author, "author is correct")
+    })
+
+    it('allows users to tip images', async () => {
+      // Track the author balance before purchase
+      let oldAuthorBalance
+      oldAuthorBalance = await web3.eth.getBalance(author)
+      oldAuthorBalance = new web3.utils.BN(oldAuthorBalance)
+
+      result = await decentragram.tipUser(postCount, { from: tipper, value: web3.utils.toWei('1', 'Ether') })
+
+      // Success
+      const event = result.logs[0].args
+      assert.equal(event.id.toNumber(), postCount.toNumber(), 'id is correct')
+      assert.equal(event.hash, hash, 'hash is correct')
+      assert.equal(event.description, 'post description', 'description is correct')
+      assert.equal(event.tipAmount, '1000000000000000000', 'tip amount is correct')
+      assert.equal(event.author, author, 'author is correct')
+
+      // Check that author received funds
+      let newAuthorBalance
+      newAuthorBalance = await web3.eth.getBalance(author)
+      newAuthorBalance = new web3.utils.BN(newAuthorBalance)
+
+      let tipUser
+      tipUser = web3.utils.toWei('1', 'Ether')
+      tipUser = new web3.utils.BN(tipUser)
+
+      const expectedBalance = oldAuthorBalance.add(tipUser)
+
+      assert.equal(newAuthorBalance.toString(), expectedBalance.toString())
+
+      // Fail: Tries to tip a image that does not exist
+      await decentragram.tipUser(99, { from: tipper, value: web3.utils.toWei('1', 'Ether') }).should.be.rejected;
+    })
+
   })
-  
 })
